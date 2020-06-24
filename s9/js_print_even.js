80c507a2bf451589e9b9a9bad40320b1ad2be989
kollidilip <55319636+kollidilip@users.noreply.github.com>
2020-06-24 20:58:34 +0530

    js even odd example

---
+++ s9/js_print_even.js
@@ -0,0 +1,15 @@
+var n = prompt("enter a value of n to get the even numbers from 0 to n");
+var i = 0;
+while (i <= n) {
+    // check for even numbers
+    if (i === 0) {
+      console.log(i +" is neither even nor odd number");
+      i++
+    }else if (i % 2 == 0) {
+      console.log(i +" is even number");
+      i++;
+    }else {
+      console.log(i +" is odd number");
+      i++;
+    }
+}
