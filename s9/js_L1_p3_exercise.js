7408bbffc415d25ccf1c0928efb671cba63643a8
kollidilip <55319636+kollidilip@users.noreply.github.com>
2020-06-24 08:42:34 +0530

    js first excercise

---
+++ s9/js_L1_p3_exercise.js
@@ -0,0 +1,5 @@
+var lbs_value = prompt("Enter the lbs weight to be converted");
+var kgs;
+kgs = lbs_value*0.454;
+alert("The converted weight in kgs is: "+ kgs);
+console.log("successfully converted");
