6ace6d7ffc27562e113ca53219e96c60e50c18f9
Dilip Kolli <dilipkolli.92@gmail.com>
2020-07-01 09:26:46 +0530

    py basics

--- s12/jgame.js
+++ s12/jgame.js
@@ -1,1 +1,14 @@
-//alert("connected")
+
+var table= $('table tr');
+
+
+// get the colu, of thr recent click
+var col = $("table button").on('click',function(){
+    console.log($(this).closest('td').index());
+    })
+
+// get the last column with grey color
+function checkBottom(col) {
+    var getColor
+
+}
---
+++ s13/number.py
@@ -0,0 +1,26 @@
+#####################################
+#### PART 6: EXERCISE REVIEW  #######
+#####################################
+
+# Time to review all the basic data types we learned! This should be a
+# relatively straight-forward and quick assignment.
+
+###############
+## Problem 1 ##
+###############
+# Given the string:
+s = 'django'
+
+# Use indexing to print out the following:
+# 'd'
+print(s[0])
+# 'o'
+print(s[-1])
+# 'djan'
+print(s[:4])
+# 'jan'
+print(s[1:4])
+# 'go'
+print(s[-2:])
+# Bonus: Use indexing to reverse the string
+print(s[5::-1])
